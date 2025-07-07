1109c3c97ae00b25b68d6ccf25e14aed7af2ca2e
RenRGaming <RenRGaming2000@gmail.com>
2025-07-07 17:01:16 +0300

    Working Database (76.3%)
    Есть незаконченные TODO

---
+++ db/main.py
@@ -0,0 +1,10 @@
+import db
+
+if __name__ == "__main__":
+    import sys
+    import asyncio
+
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(db.upload_places())
+    asyncio.run(db.upload_reviews())
